6deb254109424e142363412ced56e7609a68d3f6
th.gab <thmsdmp@gmail.com>
2026-06-26 18:59:15 +0800

    Update gps_manager.gd

--- scripts/gps_manager.gd
+++ scripts/gps_manager.gd
@@ -7,18 +7,31 @@ var _error_callback_ref: JavaScriptObject
 @onready var latLabel = $"../userInterface/CanvasLayer/Container/Lat"
 @onready var longLabel = $"../userInterface/CanvasLayer/Container/Long"
 
+# We will use a dedicated timer instead of _process
+var update_timer: Timer
+
 func _ready() -> void:
-	# Only execute if we are running in a web browser export
 	if OS.has_feature("web"):
 		_success_callback_ref = JavaScriptBridge.create_callback(_on_gps_success)
 		_error_callback_ref = JavaScriptBridge.create_callback(_on_gps_error)
+		
+		# Setup a safe loop using a real Timer node
+		update_timer = Timer.new()
+		update_timer.wait_time = 0.5
+		update_timer.autostart = true
+		update_timer.timeout.connect(request_gps_location)
+		add_child(update_timer)
+		
+		# Fire once immediately
 		request_gps_location()
 	else:
-		latLabel.text = "NOT ON BROWRSER"
-		longLabel.text = "NOT ON BROWRSER"
+		latLabel.text = "NOT ON BROWSER"
+		longLabel.text = "NOT ON BROWSER"
 		print("GPS retrieval via JavaScriptBridge is only supported on Web builds.")
 
 func request_gps_location() -> void:
+	print("Attempting to fetch GPS...") # This will let you know it's ticking
+	
 	var window = JavaScriptBridge.get_interface("window")
 	if not window:
 		print("Failed to get window interface.")
@@ -27,22 +40,18 @@ func request_gps_location() -> void:
 		
 	var navigator = window.navigator
 	if not navigator or not navigator.geolocation:
-		latLabel.text = "FAILED TO GET GPS OR NOT SUPPORTED"
-		print("Geolocation is not supported by this browser.")
+		latLabel.text = "FAILED TO GET GPS"
+		print("Geolocation is not supported or blocked by browser security.")
 		return
 	
-	# Create JavaScript options: { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
-
 	var options = JavaScriptBridge.create_object("Object")
 	options.enableHighAccuracy = true
 	options.timeout = 5000
 	options.maximumAge = 0
 	
-	# Call navigator.geolocation.getCurrentPosition(success_callback, error_callback)
-	navigator.geolocation.getCurrentPosition(_success_callback_ref, _error_callback_ref,options)
+	navigator.geolocation.getCurrentPosition(_success_callback_ref, _error_callback_ref, options)
 
 func _on_gps_success(args: Array) -> void:
-	# args[0] contains the JavaScript 'position' object
 	var position = args[0]
 	var coords = position.coords
 	
@@ -52,16 +61,9 @@ func _on_gps_success(args: Array) -> void:
 	
 	latLabel.text = str(latitude)
 	longLabel.text = str(longitude)
-	print("Latitude: ", latitude)
-	print("Longitude: ", longitude)
-	print("Accuracy (meters): ", accuracy)
+	print("SUCCESS! Lat: ", latitude, " | Long: ", longitude, " | Accuracy: ", accuracy)
 
 func _on_gps_error(args: Array) -> void:
-	# args[0] contains the JavaScript 'GeolocationPositionError' object
 	var error = args[0]
-	print("Error code: ", error.code, " | Message: ", error.message)
-
-func _process(delta: float) -> void:
-	await get_tree().create_timer(.5).timeout
-	if OS.has_feature("web"):
-		request_gps_location()
+	latLabel.text = "GPS ERROR: " + str(error.code)
+	print("GPS Error code: ", error.code, " | Message: ", error.message)
