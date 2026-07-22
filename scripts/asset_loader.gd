87e72905990c75708183e24bd843a2c9e4640b46
th.gab <thmsdmp@gmail.com>
2026-07-22 17:02:40 +0800

    asset-fetching
    added asset fetcher for downloading assets from another repository. Useful for updating data and other information like announcements etc.
    
    Open the script "data_fetcher.gd" to see how it works. You can also read "asset_loader.gd" to see how you can interact with the imported assets.

---
+++ scripts/asset_loader.gd
@@ -0,0 +1,25 @@
+extends Node
+
+# fires when the data are updated
+func _on_asset_fetcher_asset_updated() -> void:
+	# check if sample scene exists
+	var scene_path = "user://Assets/sample_scene.tscn"
+	if not FileAccess.file_exists(scene_path):
+		print("Scene file does not exist at: ", scene_path)
+		return
+	
+	# Load the PackedScene resource from user://
+	var packed_scene = ResourceLoader.load(scene_path) as PackedScene
+	
+	if packed_scene:
+		# switch scenes
+		get_tree().change_scene_to_packed(packed_scene)
+	else:
+		print("Failed to load scene resource from: ", scene_path)
+
+func _load_scene_from_user(path:String):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
