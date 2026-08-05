86f65475d757632831966731d0fd7164337bf3d0
th.gab <thmsdmp@gmail.com>
2026-08-05 16:01:21 +0800

    added framework templates
    - tab class: handles tab switching
    - home class
    - map class
    - explore class

---
+++ classes/tab_handler.gd
@@ -0,0 +1,32 @@
+extends Node
+
+# external variables
+@export var user_interface:CanvasLayer
+@export var tab_buttons:Control
+
+@export var home_tab:Control
+@export var map_tab:Control
+@export var explore_tab:Control
+
+# internal variables
+var current_tab:Control
+
+func _ready() -> void:
+	current_tab = $"../../UI/home"
+	
+	for b:Button in tab_buttons.get_children():
+		if b is Button:
+			b.pressed.connect(_switch_tab.bind(b.name))
+
+func _switch_tab(tab):
+	var target_tab:Control
+	
+	for c:Control in user_interface.get_children(true):
+		if c is Control and c.name == tab:
+			target_tab = c
+			
+			if current_tab:
+				current_tab.exit()
+			
+			target_tab.enter()
+			current_tab = target_tab
